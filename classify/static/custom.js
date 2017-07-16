db1a4ee3d3806a012e56bef5bf12a1f4943c9eb9
Dan Mo <rawrzors@users.noreply.github.com>
2017-07-16 10:25:23 -0400

    Adds file validation to jpg only

--- classify/static/custom.js
+++ classify/static/custom.js
@@ -55,6 +55,21 @@ $(document).ready( function() {
 		}));
 	});	
 	$("#imgInp").change(function(){
+	      var file = $(this).val();
+	      var exts = ['jpg'];//extensions
+	      //the file has any value?
+	      if ( file ) {
+		// split file name at dot
+		var get_ext = file.split('.');
+		// reverse name to check extension
+		get_ext = get_ext.reverse();
+		// check file type is valid as given in 'exts' array
+		if ( $.inArray ( get_ext[0].toLowerCase(), exts ) > -1 ){
+		} else {
+		  alert( 'Invalid file!' );
+		  return false
+		}
+	      }
 	    readURL(this);
 	}); 	
 });	
